60f5d8a83f0267cf0ca22d409fa39cc2240b2add
Akay0s <konstantinbibl325@mail.ru>
2025-03-27 20:56:08 +0300

    po4ti konec2.0

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -56,8 +56,8 @@ public class User implements UserDetails {
         return userName;
     }
 
-    public void setUserName(String name) {
-        this.userName = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     @Override
@@ -65,16 +65,16 @@ public class User implements UserDetails {
         return password;
     }
 
-    public void setPassword(String lastName) {
-        this.password = lastName;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String age) {
-        this.email = age;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Set<Role> getRoles() {
